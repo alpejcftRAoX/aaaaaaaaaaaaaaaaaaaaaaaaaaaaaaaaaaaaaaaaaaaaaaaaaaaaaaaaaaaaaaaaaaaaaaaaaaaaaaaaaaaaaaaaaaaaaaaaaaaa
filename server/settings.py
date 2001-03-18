0a2359a817fecfd5f9d0d2c2761f2746a07cb158
k773 <lollinux_borealis@outlook.com>
2001-03-18 20:08:18 +0300

    Almost done...

---
+++ server/settings.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# display_window_size_half = np.array([1920 // 2 + 10, 1080 // 2 + 10])
+display_window_size_half = np.array([2560 // 2 + 10, 1440 // 2 + 10])
+map_dim = 2000
+food_amount = 40
+food_radius = 20
+projectile_lifetime = 10
